1e6ea51de2618efc9c54dc4b02ba225a69667b4b
Jonathan Rupp <jrupp@rightpoint.com>
2025-03-31 19:24:29 -0500

    Add tooltip with score breakdown

--- src/app/[sport]/[league]/[date]/page.tsx
+++ src/app/[sport]/[league]/[date]/page.tsx
@@ -1,4 +1,5 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { analyzeGame } from "@/services/analysis";
 import { getGame, getScoreboard } from "@/services/espn";
 import Link from "next/link";
@@ -16,6 +17,7 @@ const CalendarPage = async ({ params }: {params: Promise<{ sport: string, league
         };
     }));
     return (
+        <TooltipProvider>
         <div>
             <h1>Calendar for {date}</h1>
             <Table>
@@ -40,7 +42,18 @@ const CalendarPage = async ({ params }: {params: Promise<{ sport: string, league
                                 <TableCell><Link href={`/${sport}/${league}/${date}/${event.id}`}>{event.shortName}</Link></TableCell>
                                 <TableCell>{t1.homeAway}: {t1.curatedRank.current} {t1.team.displayName}</TableCell>
                                 <TableCell>{t2.homeAway}: {t2.curatedRank.current} {t2.team.displayName}</TableCell>
-                                <TableCell>{event.analysis.score.toFixed(2)}</TableCell>
+                                <TableCell>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            {event.analysis.score.toFixed(2)}
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <pre>{JSON.stringify(event.analysis, null, 2)}</pre>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TableCell>
+                                <TableCell>{event.analysis.avgChangePerPlay.toFixed(2)}</TableCell>
+                                <TableCell>{event.analysis.maxLoserWPAfter90Pct.toFixed(0)}</TableCell>
                                 {/* <TableCell><pre>{JSON.stringify(event.analysis, null, 2)}</pre></TableCell> */}
                                 {/* {Object.entries(event.analysis).map(([key, value]) => (
                                     <TableCell key={key}>{
@@ -58,6 +71,7 @@ const CalendarPage = async ({ params }: {params: Promise<{ sport: string, league
             </Table>
             <pre>{JSON.stringify(events, null, 2)}</pre>
         </div>
+        </TooltipProvider>
     );
 }
 

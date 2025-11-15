cf31d21b651a43547565b1674e306d170fb60992
Jonathan Rupp <jrupp@rightpoint.com>
2025-11-15 08:50:20 -0600

    add indicator to current week

--- src/app/[sport]/[league]/page.tsx
+++ src/app/[sport]/[league]/page.tsx
@@ -6,6 +6,7 @@ const CalendarPage = async ({ params }: {params: Promise<{ sport: string, league
     const { sport, league } = await params;
     const scoreboard = await getScoreboard(sport, league);
     const calendar = scoreboard.leagues[0].calendar;
+    const now = new Date();
     return (
         <div>
             <h1>Calendar</h1>
@@ -16,7 +17,10 @@ const CalendarPage = async ({ params }: {params: Promise<{ sport: string, league
                     </li> : <Fragment key={ix}>
                         {date.entries.map(entry => (
                             <li key={entry.value}>
-                                <Link href={`/${sport}/${league}/week/${entry.value}`} className="text-blue-500 hover:underline">{date.label}: {entry.label} ({new Date(entry.startDate).toLocaleDateString()} - {new Date(entry.endDate).toLocaleDateString()})</Link>
+                                <Link href={`/${sport}/${league}/week/${entry.value}`} className="text-blue-500 hover:underline">
+                                    {date.label}: {entry.label} ({new Date(entry.startDate).toLocaleDateString()} - {new Date(entry.endDate).toLocaleDateString()})
+                                    {new Date(entry.startDate) < now && now < new Date(entry.endDate) ? ' (Current)' : ''}
+                                </Link>
                             </li>
                         ))}
                     </Fragment>
